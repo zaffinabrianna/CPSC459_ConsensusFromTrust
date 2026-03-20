86ae2447b51c24a18a67b84747ab8d1d91337033
Brianna Zaffina <83510513+zaffinabrianna@users.noreply.github.com>
2026-03-19 17:59:33 -0700

    Add files via upload

--- CompliantNode.java
+++ CompliantNode.java
@@ -4,37 +4,46 @@ import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
+    // array to keep track of nodes that we trust
     private boolean[] followees;
+    // set that holds all transactions that the network agrees with
     private Set<Transaction> proposals; 
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) 
     {
+        // empty set for transactions
         proposals = new HashSet<Transaction>();
     }
 
     public void setFollowees(boolean[] followees) 
     {
+        // save the variable that we follow
         this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) 
     {
+        // add all transactions into prosopals
         proposals.addAll(pendingTransactions);
     }
 
     public Set<Transaction> getProposals() 
     {
+        // return the current set
         return proposals;
     }
 
     public void receiveCandidates(ArrayList<Integer[]> candidates) 
     {
+        // for loop that gives list of transactions that other nodes are proposing
         for (Integer[] candidate : candidates)
         {
             int txId = candidate[0];
             int senderId = candidate[1];
+            // if sender is one that we follow then accept the transaction
             if (followees[senderId])
             {
+                // if its known, we add it to the current set
                 proposals.add(new Transaction(txId));
             }
         }
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Section:
+CPSC 459-01 18227
+
+# Names:
+Brianna Zaffina 
+Dylan Geraci
+Noah Sanderson
+
+# Emails:
+zaffinabrianna@csu.fullerton.edu
+dgeraci27@csu.fullerton.edu
+Noahs7647@csu.fullerton.edu
